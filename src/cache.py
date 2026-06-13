6caacf558b8542cd8013f6440d874234f077527a
hermoine4422-create <hermoine4422@gmail.com>
2026-06-13 16:22:11 +0530

    Update cache.py

--- src/cache.py
+++ src/cache.py
@@ -1,1 +1,14 @@
-cache = {}
+from collections import deque
+import time
+
+class SimpleCache:
+  def __init__(self, max_size=5):
+    self.items = deque(maxlen=max_size) 
+    
+    def add(self, value): 
+      self.items.append((time.time(), value)) 
+      
+      def latest(self): 
+      if not self.items:
+        return None 
+        return self.items[-1][1]
